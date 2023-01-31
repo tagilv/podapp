213c8001d90867a885bb7f19b2021531d7238b23
Viktor Tagil <viktor.tagil@gmail.com>
2023-01-31 22:45:32 +0100

    test12

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey || "mock-key",
+  apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
